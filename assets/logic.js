af7510812b67423ac9191ab8e056292a0bd1bf89
kitt mattison <kittmatt1@gmail.com>
2020-03-22 20:35:41 -0700

    added giphy api

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,14 @@
-$(document).read(){
-
-
-     var endPoint = 'https://api.giphy.com/v1/gifs/search?';
-     var apiKey = 'api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9';
-     var search = '&q=Ireland&limit=100';
-
-     var queryURL = endPoint + apiKey + search;
-
-     var req = {
-          url: queryURL,
-          method: 'GET'
-     };
-
-}
+$(document).ready(function () {
+     // my giphyApi including prefix and limit
+     var giphyApi = "api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10"
+   
+     // setting up giphy queryURL variable with beginning of url
+     var beginningURL = "https://api.giphy.com/v1/gifs/search?q=";
+   
+     // setting up array with strings for buttons
+     var topics = ["office", "tired", "coffee", "yawn", "meeting", "bored", "passive aggressive", "emails", "late", "computer crash", "purell", "water cooler"];
+   
+     // empty variable for button added by user
+     var newButton = "";
+   
+});
